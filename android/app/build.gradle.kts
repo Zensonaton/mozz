042ec3118dd5307a5d3f48a2232575e8cce5c215
Zensonaton <zensonaton@gmail.com>
2025-04-08 18:45:14 +0500

    Bump версии ndk.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.zensonaton.mozz"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
